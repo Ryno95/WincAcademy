a7a5a5ab4d500e4f6f7476793e513164df19a7f4
Ryno <58690325+Ryno95@users.noreply.github.com>
2020-04-23 10:48:06 +0200

    Data Added

---
+++ Python/Excercises/007-lists-and-list-operations/main.py
@@ -0,0 +1,29 @@
+Shane = ["Shane Embury", "Bass"]  # 1987-Present
+Mark = ["Mark Greenway", "Vocals"]  # 1989-1996, 1997-present
+Danny = ["Danny Herrera", "Drums"]  # 1991 - present
+Mitch = ["Mitch Harris", "Guitar"]  # 1990-2014
+
+Nicholas = ["Nicholas Bullen", "Vocals"]  # 1981-1986
+NicolasBass = ["Nicholas Bullen", "Bass"]  # 1981-1986
+Miles = ["Miles Ratledge", "Drums"]  # 1981–1985
+Simon = ["Simon Oppenheimer", "Guitar"]  # 1981–1982
+Graham = ["Graham Robertson", "Guitar"]  # 1982–1985
+Daryl = ["Daryl Fedeski", "Guitar"]  # 1982
+Finbar = ["Finbar Quinn", "Bass"]  # 1983–1984
+Marian = ["Marian Williams", "Vocals"]  # 1984
+Damien = ["Damien Errington", "Guitar"]  # 1985
+Justin = ["Justin Broadrick", "Guitar"]  # 1985–1986
+JustinVocals = ["Justin Broadrick", "Vocals"]  # 1985–1986
+Peter = ["Peter Shaw", "Bass"]  # 1985
+Mick = ["Mick Harris", "Drums"]  # 1985-1991
+Jim = ["Jim Whitely", "Bass"]  # 1986–1987
+Frank = ["Frank Healy", "Guitar"]  # 1986
+Bill = ["Bill Steer", "Guitar"]  # 1987–1989
+Lee = ["Lee Dorrian", "Vocals"]  # 1987–1989
+Jesse = ["Jesse Pintado", "Guitar"]  # 1989–2004
+Phil = ["Phil Vane", "Vocals"]  # 1996-1997
+Erik = ["Erik Burke", "Guitar"]  # 2015
+Jesper = ["Jesper Liverod", "Bass"]  # 2017
+
+Band = [Nicholas, NicolasBass, Simon, Miles]
+print(Band)
