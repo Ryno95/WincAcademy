a8aeb3aa13992230dcdf5d70d44c19c23a2d6729
Ryno <58690325+Ryno95@users.noreply.github.com>
2020-04-30 12:02:47 +0200

    All players list added, goalsScored list added

--- Python/Excercises/010-Functions/main.py
+++ Python/Excercises/010-Functions/main.py
@@ -42,3 +42,28 @@ for player in vitessePlayers:
         j += 1
 
 print(allPlayersDict)
+
+
+"""**doelen-list**
+
+- maak een list van de goals in deze wedstrijd
+- de goals zijn op chronologische volgorde
+- elk item in deze list is een dict met de volgende keys:
+    - id van de speler die scoorde
+    - tijdstip"""
+
+goalsScored = [
+    {"goalScorer": "Johan Neeskens", "time": 10},
+    {"goalScorer": "Johan Neeskens", "time": 28},
+    {"goalScorer": "Johan Cruyff", "time": 32},
+    {"goalScorer": "Johan Cruyff", "time": 42},
+    {"goalScorer": "Johan Cruyff", "time": 47},
+    {"goalScorer": "Dick van Dijk", "time": 49},
+    {"goalScorer": "Dick van Dijk", "time": 51},
+    {"goalScorer": "Gerrie Mühren", "time": 63},
+    {"goalScorer": "Barry Hulshoff", "time": 70},
+    {"goalScorer": "Herman Veenendaal", "time": 75},
+    {"goalScorer": "Johan Cruyff", "time": 78},
+    {"goalScorer": "Dick van Dijk", "time": 81},
+    {"goalScorer": "Johan Neeskens", "time": 88},
+]
